ea5cee4bf21a3841891c760625c0fd0d0704db01
GanjiSaiDeekshith <130652600+GanjiSaiDeekshith@users.noreply.github.com>
2026-03-21 18:11:48 +0530

    Extend BusReservationApplication for servlet support 21/03

--- src/main/java/com/bus/BusReservationApplication.java
+++ src/main/java/com/bus/BusReservationApplication.java
@@ -2,9 +2,16 @@ package com.bus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BusReservationApplication {
+public class BusReservationApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BusReservationApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusReservationApplication.class, args);
